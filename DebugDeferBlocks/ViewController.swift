6109831eab6f1be00f1f92b3b840bcb3bcbc1ccd
Eric Internicola <intere@gmail.com>
2018-10-05 11:52:35 -0600

    Added the source that demonstrates the issue.

--- DebugDeferBlocks/ViewController.swift
+++ DebugDeferBlocks/ViewController.swift
@@ -12,7 +12,12 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        defer {
+            print("Defer breakpoint")
+        }
+
+        print("Non-defer breakpoint")
     }
 
 
